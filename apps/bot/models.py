cab3319a78d87f7280515507683a13caccfa39b0
reza <adamak.tnh@gmail.com>
2025-09-28 16:48:23 +0330

    added episode and session model

--- apps/bot/models.py
+++ apps/bot/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from utils.load_env import env
+from utils.utils import generate_unique_link
+
 
 class BotUpdateStatus(models.Model):
     is_update = models.BooleanField(
@@ -55,3 +58,68 @@ class Plan(models.Model):
         if self.is_unlimited():
             return f"{self.name} - Unlimited"
         return f"{self.name} - {self.duration_days} days"
+
+class Session(models.Model):
+    """
+    Represents a movie or a series (as a whole).
+    """
+    CONTENT_TYPE_CHOICES = (
+        ("movie", "Movie"),
+        ("series", "Series"),
+    )
+
+    title = models.CharField(max_length=255)
+    content_type = models.CharField(max_length=10, choices=CONTENT_TYPE_CHOICES)
+    link = models.CharField(max_length=20, unique=True, blank=True)  # unique shareable link
+    view_count = models.PositiveIntegerField(default=0)  # total views
+    like_count = models.PositiveIntegerField(default=0)
+    dislike_count = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.content_type})"
+
+    def get_link(self):
+        return f"https://t.me/{env.BOT_USERNAME}/?start={self.link}"
+
+    def save(self, *args, **kwargs):
+        if not self.link:
+            self.link = generate_unique_link("S")  # auto generate if empty
+        super().save(*args, **kwargs)
+
+class Episode(models.Model):
+    """
+    Episodes of a series. Only used if Content.type == 'series'.
+    """
+    link = models.CharField(max_length=20, unique=True, blank=True)  # unique shareable link
+    session = models.ForeignKey(Session, on_delete=models.CASCADE, related_name="episodes")
+    # title = models.CharField(max_length=255)
+    message_id = models.BigIntegerField(null=True, blank=True)  # Telegram message_id in backup channel
+    order = models.PositiveIntegerField(blank=True, null=True)  # for ordering episodes
+
+    def save(self, *args, **kwargs):
+        if self._state.adding: # just if added new records
+            if not self.link:
+                self.link = generate_unique_link("E")
+            self.order = self.calc_order(self.session.id)
+        super().save(*args, **kwargs)
+
+    @staticmethod
+    def calc_order(session_id):
+        present_orders = (
+            Episode.objects.filter(session_id=session_id)
+            .order_by("-order")
+            .values_list("order", flat=True)
+        )
+        if present_orders:
+            return present_orders[0] + 1
+        return 1
+
+    class Meta:
+        ordering = ["order"]
+
+    def __str__(self):
+        return f"{self.order} (Episode of {self.session.title})"
+
+    def get_link(self):
+        return f"https://t.me/{env.BOT_USERNAME}/?start={self.link}"
